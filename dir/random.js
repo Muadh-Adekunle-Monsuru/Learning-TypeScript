c17bd97d8e9d20c25dfedcdd94addfc8bd1e289c
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2024-03-02 21:42:11 +0100

    d

---
+++ dir/random.js
@@ -0,0 +1,9 @@
+"use strict";
+function generateRandomNumber(min, max) {
+    // Generate a random decimal between 0 (inclusive) and 1 (exclusive)
+    const randomDecimal = Math.random();
+    // Scale and shift the decimal to fit within the desired range
+    const randomNumber = Math.floor(randomDecimal * (max - min + 1) + min);
+    return randomNumber;
+}
+console.log(generateRandomNumber(1300, 2800));
